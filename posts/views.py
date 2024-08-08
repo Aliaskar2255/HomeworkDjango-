096de9a1585b5413bc9c4eafb62584abe8a4b3b0
 ÐAliaskar2255 <aliaskar2255@gmail.com>
2024-08-08 22:29:21 +0600

    123

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from posts.models import Post
 
 def word(request):
     return HttpResponse('Hello my friend. You play dota 2?')
 
 def main_page(request):
     return render(request, template_name='main_page.html')
+
+def post_list_view(request):
+    posts = Post.objects.all()
+    return render(request, template_name='post_list.html', context={'posts': posts})
+
+def post_detail_view(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'post_detail.html', context={'post': post})
